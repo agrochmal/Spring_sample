0d6074c7df61ea2415c808c782bf7816a36638a5
Robert Sikora <robertsikora@outlook.com>
2015-07-30 18:13:53 +0200

    [refactoring] search advert service

--- src/main/java/pl/demo/core/service/SearchAdvertServiceImpl.java
+++ src/main/java/pl/demo/core/service/SearchAdvertServiceImpl.java
@@ -2,6 +2,7 @@ package pl.demo.core.service;
 
 import org.apache.lucene.search.BooleanClause;
 import org.apache.lucene.search.BooleanQuery;
+import org.apache.lucene.search.Query;
 import org.hibernate.search.jpa.FullTextEntityManager;
 import org.hibernate.search.query.dsl.QueryBuilder;
 import org.hibernate.search.query.dsl.Unit;
@@ -10,6 +11,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
+import org.springframework.util.Assert;
 import pl.demo.core.model.entity.Advert;
 import pl.demo.web.dto.SearchCriteriaDTO;
 
@@ -28,41 +30,19 @@ public class SearchAdvertServiceImpl implements SearchAdvertService {
     private EntityManagerFactory entityManagerFactory;
 
     public Page<Advert> searchAdverts(final SearchCriteriaDTO searchCriteriaDTO, final Pageable pageable) {
-        final EntityManager em = entityManagerFactory.createEntityManager();;
+        Assert.notNull(searchCriteriaDTO, "searchCriteriaDTO is required");
+        Assert.notNull(pageable, "pageable is required");
+
+        final EntityManager em = entityManagerFactory.createEntityManager();
         try {
             final FullTextEntityManager fullTextEntityManager = org.hibernate.search.jpa.Search.getFullTextEntityManager(em);
             em.getTransaction().begin();
 
             final QueryBuilder qb = fullTextEntityManager.getSearchFactory().buildQueryBuilder().forEntity(Advert.class).get();
-
-            org.apache.lucene.search.Query locationQuery = null;
-            org.apache.lucene.search.Query keywordQuery = null;
-
-            if (searchCriteriaDTO.hasLocation()) {
-                final double radius = searchCriteriaDTO.getLocRadius();
-                locationQuery = qb.spatial()
-                        .onDefaultCoordinates()
-                        .within(radius, Unit.KM)
-                        .ofLatitude(searchCriteriaDTO.getLocLatitude())
-                        .andLongitude(searchCriteriaDTO.getLocLongitude())
-                        .createQuery();
-            }
-
-            if (searchCriteriaDTO.hasKeyword()) {
-                keywordQuery = qb
-                        .keyword()
-                        .onFields("title", "description")
-                        .matching(searchCriteriaDTO.getKeyWords())
-                        .createQuery();
-            }
-
-            final BooleanQuery bq = new BooleanQuery();
-            if (null != locationQuery) {
-                bq.add(new BooleanClause(locationQuery, BooleanClause.Occur.MUST));
-            }
-            if (null != keywordQuery) {
-                bq.add(new BooleanClause(keywordQuery, BooleanClause.Occur.MUST));
-            }
+            final BooleanQuery bq = new CreateriaBuilder(searchCriteriaDTO, qb)
+                    .applyLocationCreateria()
+                    .applyKeywordCreateria()
+                    .build();
             final javax.persistence.Query jpaQuery = fullTextEntityManager.createFullTextQuery(bq, Advert.class);
             final int total = jpaQuery.getResultList().size();
             final int pageSize = pageable.getPageSize();
@@ -73,7 +53,79 @@ public class SearchAdvertServiceImpl implements SearchAdvertService {
             final List result = jpaQuery.getResultList();
             em.getTransaction().commit();
 
-            return new Page<Advert>() {
+            return new PageBuilder<Advert>(result, total, pageable).build();
+
+        }finally {
+            em.close();
+        }
+    }
+
+    private static final class CreateriaBuilder{
+
+        private final SearchCriteriaDTO searchCriteriaDTO;
+        private final QueryBuilder qb;
+        private Query locationQuery = null;
+        private Query keywordQuery = null;
+
+        public CreateriaBuilder(final SearchCriteriaDTO searchCriteriaDTO, final QueryBuilder qb) {
+            this.searchCriteriaDTO = searchCriteriaDTO;
+            this.qb = qb;
+        }
+
+        public CreateriaBuilder applyKeywordCreateria(){
+            if (searchCriteriaDTO.hasKeyword()) {
+                keywordQuery = qb
+                        .keyword()
+                        .onFields("title", "description")
+                        .matching(searchCriteriaDTO.getKeyWords())
+                        .createQuery();
+            }
+            return this;
+        }
+
+        public CreateriaBuilder applyLocationCreateria(){
+            if (searchCriteriaDTO.hasLocation()) {
+                final double radius = searchCriteriaDTO.getLocRadius();
+                locationQuery = qb.spatial()
+                        .onDefaultCoordinates()
+                        .within(radius, Unit.KM)
+                        .ofLatitude(searchCriteriaDTO.getLocLatitude())
+                        .andLongitude(searchCriteriaDTO.getLocLongitude())
+                        .createQuery();
+            }
+            return this;
+        }
+
+        public BooleanQuery build(){
+            final BooleanQuery bq = new BooleanQuery();
+            if (null != locationQuery) {
+                bq.add(new BooleanClause(locationQuery, BooleanClause.Occur.MUST));
+            }
+            if (null != keywordQuery) {
+                bq.add(new BooleanClause(keywordQuery, BooleanClause.Occur.MUST));
+            }
+            return bq;
+        }
+    }
+
+    private static final class PageBuilder<T>{
+
+        private final List result;
+        private final long total;
+        private final Pageable pageable;
+
+
+        private PageBuilder(final List result, final long total, final Pageable pageable) {
+            this.result = result;
+            this.total = total;
+            this.pageable = pageable;
+        }
+
+        public Page<T> build(){
+
+            validate();
+
+            return new Page<T>() {
                 @Override
                 public int getTotalPages() {
                     return  new Long( getTotalElements() / getSize() ).intValue() + 1;
@@ -100,7 +152,7 @@ public class SearchAdvertServiceImpl implements SearchAdvertService {
                 }
 
                 @Override
-                public List<Advert> getContent() {
+                public List<T> getContent() {
                     return result;
                 }
 
@@ -145,21 +197,14 @@ public class SearchAdvertServiceImpl implements SearchAdvertService {
                 }
 
                 @Override
-                public Iterator<Advert> iterator() {
+                public Iterator<T> iterator() {
                     return null;
                 }
             };
-
-        }finally {
-            em.close();
         }
-    }
 
-    private static final class CreateriaBuilder{
-        //to-do
-    }
-
-    private static final class PageBuider{
-        //to-do
+        private void validate(){
+            Assert.state(null!=result && result.size() != total, "Inconsistent data!");
+        }
     }
 }
