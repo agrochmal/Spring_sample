42059447d0f9ee1216efc68b75bccab22fa07cc8
Robert Sikora <robertsikora@outlook.com>
2015-09-08 19:50:33 +0200

    -rafactoring

--- src/main/java/pl/demo/web/validator/ImageUploadValidator.java
+++ src/main/java/pl/demo/web/validator/ImageUploadValidator.java
@@ -1,12 +1,10 @@
 package pl.demo.web.validator;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.validation.Errors;
 import org.springframework.validation.Validator;
 import org.springframework.web.multipart.MultipartFile;
 import pl.demo.MsgConst;
-import pl.demo.core.util.MessageResolver;
 import pl.demo.web.exception.ValidationRequestException;
 
 /**
@@ -16,10 +14,7 @@ import pl.demo.web.exception.ValidationRequestException;
 @Component
 public class ImageUploadValidator implements Validator {
 
-    private final static long MAX_FILE_SIZE = 10_000;
-
-    @Autowired
-    private MessageResolver messageResolver;
+    private final static long MAX_FILE_SIZE = 10_000_000;
 
     @Override
     public boolean supports(Class<?> clazz) {
@@ -33,10 +28,10 @@ public class ImageUploadValidator implements Validator {
     public void validate(final Object target){
         final MultipartFile multipartFile = (MultipartFile)target;
         if(multipartFile.isEmpty()){
-            throw new ValidationRequestException(messageResolver.getMessage(MsgConst.FILE_EMPTY));
+            throw new ValidationRequestException(MsgConst.FILE_EMPTY);
         }
         if(multipartFile.getSize() > MAX_FILE_SIZE){
-            throw new ValidationRequestException(messageResolver.getMessage(MsgConst.FILE_TOO_BIG));
+            throw new ValidationRequestException(MsgConst.FILE_TOO_BIG);
         }
     }
 }
